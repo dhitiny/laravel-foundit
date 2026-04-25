6d411d9206cd4dddc226191a303fd353ee4d996a
Anandita Syahrani Putri <diiwon93@gmail.com>
2026-04-25 20:16:45 +0700

    benerin barang.php

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     protected $table = 'barang';
-<<<<<<< HEAD
-    protected $primaryKey = 'id_item';
+    protected $primaryKey = 'id_item';git
     public $timestamps = false;
 
     protected $fillable = [
@@ -22,8 +21,8 @@ class Barang extends Model
         'status_admin', // Kolom baru yang kamu buat di phpMyAdmin
         'foto_barang',
     ];
-=======
+
 protected $primaryKey = 'id_item';
 protected $fillable = ['id_kategori', 'nama_barang', 'deskripsi', 'status']; 
->>>>>>> fitur-filter
+
 }
